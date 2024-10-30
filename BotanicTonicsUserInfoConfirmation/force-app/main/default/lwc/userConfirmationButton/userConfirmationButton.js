14a0a0d5df1ec2cb3887ea83b061b7b92e119d2d
Jesper Rapouw <rapouw@gmail.com>
2024-10-30 08:28:58 -0700

    function fullyworks,now its to clean up code

--- BotanicTonicsUserInfoConfirmation/force-app/main/default/lwc/userConfirmationButton/userConfirmationButton.js
+++ BotanicTonicsUserInfoConfirmation/force-app/main/default/lwc/userConfirmationButton/userConfirmationButton.js
@@ -1,38 +1,27 @@
 import { LightningElement, api, track } from 'lwc';
-import { FlowNavigationNextEvent } from 'lightning/flowSupport';
 
 export default class UserConfirmationButton extends LightningElement {
   @api recordId;
   @track isFlowVisible = false;
 
-  // Prepare flow input variable for recordId
   get flowInputs() {
     return [
       {
         name: "recordId",
         type: "String",
         value: this.recordId
-      }
+      },
     ];
   }
 
-  // Show the modal with the flow
   handleClick() {
     this.isFlowVisible = true;
   }
 
-  // Close the modal without affecting the flow
   closeModal() {
     this.isFlowVisible = false;
   }
 
-  // Handle Confirm button click to continue to the next step in the flow
-  handleConfirm() {
-    const flowElement = this.template.querySelector("lightning-flow");
-    flowElement.dispatchEvent(new FlowNavigationNextEvent());
-  }
-
-  // Close modal when flow finishes
   handleStatusChange(event) {
     if (event.detail.status === "FINISHED") {
       this.isFlowVisible = false;
