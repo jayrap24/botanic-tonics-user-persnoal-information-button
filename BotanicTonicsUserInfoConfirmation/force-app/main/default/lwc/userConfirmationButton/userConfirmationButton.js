be85b8d74ac42297a980a5f1c1720c77f8453e7c
Jesper Rapouw <rapouw@gmail.com>
2024-10-30 13:12:44 -0700

    in testing mode by Adam

--- BotanicTonicsUserInfoConfirmation/force-app/main/default/lwc/userConfirmationButton/userConfirmationButton.js
+++ BotanicTonicsUserInfoConfirmation/force-app/main/default/lwc/userConfirmationButton/userConfirmationButton.js
@@ -1,8 +1,11 @@
 import { LightningElement, api, track } from 'lwc';
+import { getRecordNotifyChange } from 'lightning/uiRecordApi';
+// import { notifyRecordUpdateAvailable } from 'lightning/uiRecordApi'
 
 export default class UserConfirmationButton extends LightningElement {
   @api recordId;
-  @track isFlowVisible = false;
+  // @track isFlowVisible = false;
+  isFlowVisible = false;
 
   get flowInputs() {
     return [
@@ -25,6 +28,10 @@ export default class UserConfirmationButton extends LightningElement {
   handleStatusChange(event) {
     if (event.detail.status === "FINISHED") {
       this.isFlowVisible = false;
+      // window.location.reload();
+      // Notify Salesforce to refresh the record data
+      getRecordNotifyChange([{ recordId: this.recordId }]);
+      // notifyRecordUpdateAvailable({ recordId: this.recordId });
     }
   }
 }
